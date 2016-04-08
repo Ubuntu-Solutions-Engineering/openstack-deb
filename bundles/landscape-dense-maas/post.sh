08bc373de752ca87921302fb9f55b010c29f0f17
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-08 11:01:43 -0400

    fix landscape registration
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bundles/landscape-dense-maas/post.sh
+++ bundles/landscape-dense-maas/post.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -ex
 
+landscape_exec=/usr/share/openstack/bundles/landscape-dense-maas/configure-landscape
+hostname=`$landscape_exec --admin-email "root@example.com" --admin-name "administrator" --system-email "root@example.com" --maas-host "$MAAS_SERVER" --maas-apikey "$MAAS_OAUTH"`
 
-./configure-landscape --admin-email "root@example.com" \
-                      --admin-name "administrator" \
-                      --system-email "root@example.com" \
-                      --maas-host "$MAAS_SERVER" \
-                      --maas-apikey "$MAAS_OAUTH"
+RET=$?
+if [ $RET -ne 0 ]; then
+    result_bool="false"
+    result=$RET
+    result_message="Failed to register, maybe Landscape is not quite up yet."
+else
+    result=0
+    result_bool="true"
+    result_message="Finish registration by visiting http://$hostname/account/standalone/openstack with Email: root@example.com Password: changeMe12345"
+fi
 
-
-printf '{"message": "%s", "returnCode": %d, "isComplete": %s}' "Registered against autopilot" $? "true"
+printf '{"message": "%s", "returnCode": %d, "isComplete": %s}' "$result_message" $result "$result_bool"
