8722bc60dd802ef47b03eda7d567defebb60b283
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-16 18:00:48 -0400

    update scripts
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bundles/openstack-base/post.sh
+++ bundles/openstack-base/post.sh
@@ -5,19 +5,19 @@
 
 if [[ $JUJU_PROVIDERTYPE =~ "lxd" ]]; then
     debug openstack "(post) setting osd-devices for ceph"
-    juju set-config ceph "osd-devices=/opt/ceph-osd" || \
-        { exposeResult "Unable to set ceph device yet, retrying.." 1 "false"; exit 1; }
+    juju set-config ceph "osd-devices=/opt/ceph-osd" 2> /dev/null
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        exposeResult "Unable to set ceph device yet, retrying.." $RET "false"
+        exit 0
+    fi
 
-    juju set-config ceph-osd "osd-devices=/opt/ceph-osd" || \
-        { exposeResult "Unable to set ceph device yet, retrying.." 1 "false"; exit 1; }
-
-    $SCRIPTPATH/neutron-ext-net -g 192.168.21.1 \
-                                -c 192.168.21.0/24 \
-                                -f 192.168.21.100:192.168.21.200 ext_net || \
-        { exposeResult "Unable to create ext-net, retrying.." 1 "false"; exit 1; }
-
-    $SCRIPTPATH/neutron-tenant-net -t admin -r provider-router admin_net 10.5.5.0/24 || \
-        { exposeResult "Unable to create admin-net, retrying.." 1 "false"; exit 1; }
+    juju set-config ceph-osd "osd-devices=/opt/ceph-osd" 2> /dev/null
+    RET=$?
+    if [ $RET -ne 0 ];then
+        exposeResult "Unable to set ceph device yet, retrying.." $RET "false"
+        exit 0
+    fi
 fi
 
 dashboard_address=$(unitAddress openstask-dashboard 0)
@@ -26,5 +26,5 @@ if [ $dashboard_address != "null" ]; then
     exit 0
 else
     exposeResult "Waiting for the dashboard to become available..." 1 "false"
-    exit 1
+    exit 0
 fi
