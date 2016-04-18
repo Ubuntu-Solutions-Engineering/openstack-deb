1e1c965d479941cc1f64e9192ab314ae6096547f
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-18 11:57:29 -0400

    add lxd support
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ bundles/openstack-lxd/post-bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. /usr/share/conjure-up/hooklib/common.sh
+
+if [[ $JUJU_PROVIDERTYPE =~ "lxd" ]]; then
+    debug openstack "(post-bootstrap) processing lxd"
+
+    profilename=$(juju switch | cut -d: -f2)
+    sed "s/##MODEL##/$profilename/" $SCRIPTPATH/lxd-profile.yaml | lxc profile edit "juju-$profilename"
+
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        exposeResult "(post-bootstrap) Failed to udate lxd profile" $RET "false"
+        exit 0
+    else
+        exposeResult "(post-bootstrap) Complete" 0 "true"
+        exit 0
+    fi
+else
+    debug openstack "(post-bootstrap) unknown provider type $JUJU_PROVIDERTYPE"
+
+    exposeResult "Unknown provider type" 1 "false"
+    exit 0
+fi
