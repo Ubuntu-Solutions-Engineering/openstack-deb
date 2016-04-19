ca074a793666a9937e6284ad7a6bb515631042db
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-19 10:45:24 -0400

    move profile updates to pre hook
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bundles/openstack-base/pre.sh
+++ bundles/openstack-base/pre.sh
@@ -3,20 +3,19 @@
 . /usr/share/conjure-up/hooklib/common.sh
 
 if [[ $JUJU_PROVIDERTYPE =~ "lxd" ]]; then
-    debug openstack "(post-bootstrap) processing lxd"
+    debug openstack "(pre) processing lxd"
 
     profilename=$(juju switch | cut -d: -f2)
     sed "s/##MODEL##/$profilename/" $SCRIPTPATH/lxd-profile.yaml | lxc profile edit "juju-$profilename"
 
     RET=$?
     if [ $RET -ne 0 ]; then
-        exposeResult "(post-bootstrap) Failed to udate lxd profile" $RET "false"
+        exposeResult "(pre) Failed to udate lxd profile" $RET "false"
     else
-        exposeResult "(post-bootstrap) Complete" 0 "true"
+        exposeResult "(pre) Complete" 0 "true"
     fi
-
 else
-    debug openstack "(post-bootstrap) unknown provider type $JUJU_PROVIDERTYPE"
+    debug openstack "(pre) unknown provider type $JUJU_PROVIDERTYPE"
 
     exposeResult "Unknown provider type" 1 "false"
 fi
