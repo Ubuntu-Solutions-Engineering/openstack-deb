45188ab113a52a400e08f47af5ea26b3ad2277e9
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-23 16:51:09 -0400

    cleanup neutron code
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bundles/bundle-common.sh
+++ bundles/bundle-common.sh
@@ -7,69 +7,65 @@ fail_cleanly() {
 # NEUTRON
 # Configures neutron
 config_neutron() {
-    debug openstack "adding ext-net"
-    neutron net-show ext-net >> /dev/null || neutron net-create --router:external ext-net >> /dev/null
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to create external router..."
-    fi
-    debug openstack "adding ext-subnet"
-    neutron subnet-show ext-subnet >> /dev/null || neutron subnet-create --name ext-subnet ext-net 10.99.0.0/24 \
-                                              --gateway 10.99.0.1 \
-                                              --allocation-pool start=10.99.0.2,end=10.99.0.254 >> /dev/null
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to create external subnet..."
+    if ! neutron net-show ext-net > /dev/null 2>&1; then
+        debug openstack "adding ext-net"
+        if ! neutron net-create --router:external ext-net > /dev/null 2>&1; then
+            fail_cleanly "Neutron unable to create external router..."
+        fi
     fi
 
-    debug openstack "adding ubuntu-net"
-    neutron net-show ubuntu-net >> /dev/null || neutron net-create ubuntu-net --shared >> /dev/null
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to create network..."
+    if ! neutron subnet-show ext-subnet > /dev/null 2>&1; then
+        debug openstack "adding ext-subnet"
+        if ! neutron subnet-create --name ext-subnet ext-net 10.99.0.0/24 \
+             --gateway 10.99.0.1 \
+             --allocation-pool start=10.99.0.2,end=10.99.0.254 > /dev/null 2>&1; then
+            fail_cleanly "Neutron unable to create external subnet..."
+        fi
     fi
 
-    debug openstack "adding ubuntu-subnet"
-    neutron subnet-show ubuntu-subnet >> /dev/null || neutron subnet-create --name ubuntu-subnet \
-                                                 --gateway 10.101.0.1 \
-                                                 --dns-nameserver 10.99.0.1 ubuntu-net 10.101.0.0/24 >> /dev/null
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to create subnet..."
+    if ! neutron net-show ubuntu-net > /dev/null 2>&1; then
+        debug openstack "adding ubuntu-net"
+        if ! neutron net-create ubuntu-net --shared > /dev/null 2>&1; then
+            fail_cleanly "Neutron unable to create network..."
+        fi
     fi
 
-    debug openstack "adding ubuntu-router"
-    neutron router-show ubuntu-router >> /dev/null || neutron router-create ubuntu-router >> /dev/null
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to create router..."
+    if ! neutron subnet-show ubuntu-subnet > /dev/null 2>&1; then
+        debug openstack "adding ubuntu-subnet"
+        if ! neutron subnet-create --name ubuntu-subnet \
+             --gateway 10.101.0.1 \
+             --dns-nameserver 10.99.0.1 ubuntu-net 10.101.0.0/24 > /dev/null 2>&1; then
+            fail_cleanly "Neutron unable to create subnet..."
+        fi
     fi
 
-    debug openstack "adding ubuntu router interface"
-    neutron router-interface-add ubuntu-router ubuntu-subnet 2> /dev/null || true
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Neutron unable to add interface to Neutron router and subnet..."
+    if ! neutron router-show ubuntu-router > /dev/null 2>&1; then
+        debug openstack "adding ubuntu-router"
+        if ! neutron router-create ubuntu-router > /dev/null 2>&1; then
+            fail_cleanly "Neutron unable to create router..."
+        fi
+    fi
+
+    if ! neutron router-interface-add ubuntu-router ubuntu-subnet > /dev/null 2>&1; then
+        debug openstack "Neutron router interface already exists..."
     fi
 
     debug openstack "setting router gateway"
-    neutron router-gateway-set ubuntu-router ext-net >> /dev/null || true
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        fail_cleanly "Unable to set Neutron gateway to router..."
+    if ! neutron router-gateway-set ubuntu-router ext-net > /dev/null 2>&1; then
+        debug openstack "Neutron gateway to ubuntu-router already exists..."
     fi
 
     # create pool of at least 5 floating ips
     debug openstack "creating floating ips"
-    existingips=$(neutron floatingip-list -f csv | wc -l) # this number will include a header line
+    existingips=$(neutron floatingip-list -f csv | tail -n +2| wc -l)
     to_create=$((6 - existingips))
     i=0
     while [ $i -ne $to_create ]; do
-        neutron floatingip-create ext-net >> /dev/null
+        neutron floatingip-create ext-net > /dev/null 2>&1
         i=$((i + 1))
     done
-    # configure security groups
 
+    # configure security groups
     debug openstack "setting security roles"
     neutron security-group-rule-create --direction ingress --ethertype IPv4 --protocol icmp --remote-ip-prefix 0.0.0.0/0 default 2> /dev/null || true
     neutron security-group-rule-create --direction ingress --ethertype IPv4 --protocol tcp --port-range-min 22 --port-range-max 22 --remote-ip-prefix 0.0.0.0/0 default 2> /dev/null || true
