6f50f6edc0ad71c41cc89751c798e346b181bdb0
Adam Stokes <adam.stokes@ubuntu.com>
2016-05-19 16:01:48 -0400

    cleanup
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bundles/landscape-dense-maas/post-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /usr/share/conjure-up/hooklib/common.sh
-
-debug openstack "Running post-bootstrap for a Autopilot install"
-
-exposeResult "Post bootstrap complete" 0 "true"
--- bundles/openstack-base/pre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. /usr/share/conjure-up/hooklib/common.sh
-
-$SCRIPTPATH/../common-openstack/pre.sh
-
